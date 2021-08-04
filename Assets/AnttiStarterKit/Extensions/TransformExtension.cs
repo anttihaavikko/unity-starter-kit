7ebc110db806c4372411043bbc50360b35335486
Antti Haavikko <antti.haavikko@gmail.com>
2021-08-04 15:25:37 +0300

    Add default value for mirroring

--- Assets/AnttiStarterKit/Extensions/TransformExtension.cs
+++ Assets/AnttiStarterKit/Extensions/TransformExtension.cs
@@ -4,10 +4,10 @@ namespace AnttiStarterKit.Extensions
 {
     public static class TransformExtension
     {
-        public static void Mirror(this Transform t, bool mirror)
+        public static void Mirror(this Transform t, bool shouldMirror = true)
         {
             var scale = t.localScale;
-            t.localScale = new Vector3((mirror ? -1f : 1f) * scale.x, scale.y, scale.z);
+            t.localScale = new Vector3((shouldMirror ? -1f : 1f) * scale.x, scale.y, scale.z);
         }
     }
 }
