427e2216523dafbae399797a5a98f94b742a1dd2
Antti Haavikko <antti.haavikko@gmail.com>
2021-11-07 15:28:06 +0200

    Minor tweaks

--- Assets/Editor/SoundCompositionEditor.cs
+++ Assets/Editor/SoundCompositionEditor.cs
@@ -2,10 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using AnttiStarterKit.Managers;
 using AnttiStarterKit.ScriptableObjects;
-using Codice.Client.BaseCommands;
 using UnityEditor;
 using UnityEngine;
 
