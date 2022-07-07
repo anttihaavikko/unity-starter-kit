40cb7927e8135308862e54429a2fef80816c3be1
Antti Haavikko <antti.haavikko@gmail.com>
2022-07-07 12:18:44 +0300

    Use PermaSave on Saver

--- Assets/AnttiStarterKit/Utils/Saver.cs
+++ Assets/AnttiStarterKit/Utils/Saver.cs
@@ -9,32 +9,32 @@ namespace AnttiStarterKit.Utils
         public static void Save(object data, string keySuffix = "")
         {
             var json = JsonUtility.ToJson(data, true);
-            PermaSave.SetString(Key + keySuffix, json);
+            PlayerPrefs.SetString(Key + keySuffix, json);
         }
 
         public static T Load<T>(string keySuffix = "") where T : class
         {
-            if (!PermaSave.HasKey(Key + keySuffix)) return null;
+            if (!PlayerPrefs.HasKey(Key + keySuffix)) return null;
             
-            var json = PermaSave.GetString(Key + keySuffix);
+            var json = PlayerPrefs.GetString(Key + keySuffix);
             return JsonUtility.FromJson<T>(json);
         }
 
         public static bool Exists(string keySuffix = "")
         {
-            return PermaSave.HasKey(Key + keySuffix);
+            return PlayerPrefs.HasKey(Key + keySuffix);
         }
 
         public static void Clear(string keySuffix = "")
         {
-            if (!PermaSave.HasKey(Key + keySuffix)) return;
+            if (!PlayerPrefs.HasKey(Key + keySuffix)) return;
             
-            PermaSave.DeleteKey(Key + keySuffix);
+            PlayerPrefs.DeleteKey(Key + keySuffix);
         }
 
         public static void Debug(string keySuffix = "")
         {
-            UnityEngine.Debug.Log(PermaSave.GetString(Key + keySuffix, "No data"));
+            UnityEngine.Debug.Log(PlayerPrefs.GetString(Key + keySuffix, "No data"));
         }
 
         public static T LoadOrCreate<T>(string keySuffix = "") where T : class, new()
