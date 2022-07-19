20a574d5432bf6c8aab2a19f2983dcffa6222d5d
Antti Haavikko <antti.haavikko@gmail.com>
2022-07-19 10:58:05 +0300

    Make min/max changeable

--- Assets/AnttiStarterKit/Randomizers/RandomRotation.cs
+++ Assets/AnttiStarterKit/Randomizers/RandomRotation.cs
@@ -4,9 +4,13 @@ namespace AnttiStarterKit.Randomizers
 {
     public class RandomRotation : MonoBehaviour
     {
+        [SerializeField] private float min = 0f;
+        [SerializeField] private float max = 360f;
+        
+        
         private void Start()
         {
-            transform.rotation = Quaternion.Euler(new Vector3(0, 0, Random.Range(0, 360f)));
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, Random.Range(min, max)));
         }
     }
 }
